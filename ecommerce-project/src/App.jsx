d17d3e3a903cd01c10d089763dd038d015dc4ebd
Tije <134832799+Tije-dev@users.noreply.github.com>
2026-01-23 00:09:52 +0100

    move Oders page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Checkout } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<Checkout />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
