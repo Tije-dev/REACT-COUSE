43eeb99db967b5a99c47ce75ce3c00671319590c
Tije <134832799+Tije-dev@users.noreply.github.com>
2026-01-26 10:22:46 +0100

    generate HTML for the cart items

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -47,57 +47,50 @@ export function CheckoutPage({ cart }) {
                       </div>
                     </div>
 
-                    <div className="delivery-options">
-                      <div className="delivery-options-title">
-                        Choose a delivery option:
+                <div className="delivery-options">
+                  <div className="delivery-options-title">
+                    Choose a delivery option:
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio" checked
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Tuesday, June 21
                       </div>
-                      <div className="delivery-option">
-                        <input
-                          type="radio"
-                          checked
-                          className="delivery-option-input"
-                          name={`delivery-option-${cartItem.productId}`}
-                        />
-                        <div>
-                          <div className="delivery-option-date">
-                            Tuesday, June 21
-                          </div>
-                          <div className="delivery-option-price">
-                            FREE Shipping
-                          </div>
-                        </div>
-                      </div>
-                      <div className="delivery-option">
-                        <input
-                          type="radio"
-                          className="delivery-option-input"
-                          name={`delivery-option-${cartItem.productId}`}
-                        />
-                        <div>
-                          <div className="delivery-option-date">
-                            Wednesday, June 15
-                          </div>
-                          <div className="delivery-option-price">
-                            $4.99 - Shipping
-                          </div>
-                        </div>
-                      </div>
-                      <div className="delivery-option">
-                        <input
-                          type="radio"
-                          className="delivery-option-input"
-                          name={`delivery-option-${cartItem.productId}`}
-                        />
-                        <div>
-                          <div className="delivery-option-date">
-                            Monday, June 13
-                          </div>
-                          <div className="delivery-option-price">
-                            $9.99 - Shipping
-                          </div>
-                        </div>
+                      <div className="delivery-option-price">
+                        FREE Shipping
                       </div>
                     </div>
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Wednesday, June 15
+                      </div>
+                      <div className="delivery-option-price">
+                        $4.99 - Shipping
+                      </div>
+                    </div>
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Monday, June 13
+                      </div>
+                      <div className="delivery-option-price">
+                        $9.99 - Shipping
+                      </div>
+                    </div>
+                  </div>
+                </div>
                   </div>
                 </div>
               );
@@ -105,7 +98,9 @@ export function CheckoutPage({ cart }) {
           </div>
 
           <div className="payment-summary">
-            <div className="payment-summary-title">Payment Summary</div>
+            <div className="payment-summary-title">
+              Payment Summary
+            </div>
 
             <div className="payment-summary-row">
               <div>Items (3):</div>
