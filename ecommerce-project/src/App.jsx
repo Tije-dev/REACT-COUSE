c2a56340611fec349a5d378213fb87cf2c8e27e0
Tije <134832799+Tije-dev@users.noreply.github.com>
2026-01-28 14:24:42 +0100

    creaye add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -13,19 +13,19 @@ import "./App.css";
 
 function App() {
   // lift cart up And share it bettween pages
-  const [cart, setCart] = useState([]);
-  useEffect(() => {
-    const fetchAppData = async () => {
+  const [cart, setCart] = useState([]); 
+      const loadCart = async () => {
       const response = await axios.get("/api/cart-items?expand=product");
       setCart(response.data);
     };
 
-    fetchAppData();
+  useEffect(() => {
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking/:orderId/:productId" element={<TrackingPage />} />
